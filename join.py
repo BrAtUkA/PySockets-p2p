f2073299548c8ebbb45218b6bfa27e83ae6adc3f
BrAtUkA <56762784+BrAtUkA@users.noreply.github.com>
2021-08-12 18:01:28 +0500

    Add files via upload

---
+++ join.py
@@ -0,0 +1,56 @@
+from os import system as cmd
+from time import sleep
+import socket
+import json
+import sys
+
+try:
+    s_ip = sys.argv[1]
+    s_port = int(sys.argv[2])
+except:
+    print("\n [!] Script usage : join.py <Server_ip> <Server_port>\n      Example >> join.py localhost 1234\n")
+    s_ip = "13.76.177.227"
+    s_port = 80
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((s_ip, s_port))
+print(f"\n [+] Connected To Server >> '{s_ip}:{s_port}'")
+
+print("\n=========================< Client >=========================\n")
+
+HEADERSIZE = 20
+
+def receive_msg(sockt:socket.socket, Len:bool):
+    full_msg = ""
+    new_msg = True
+    while True:
+        msg = sockt.recv(24)
+        if msg == "":
+            raise Exception("Error ??? No Response.")
+        if new_msg:
+            if Len:
+                print(f"\n Upcoming Message Length: {int(msg[:HEADERSIZE])}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+        
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msglen:  # Check if the msg is completley sent.. using the header
+            break
+
+    full_msg = json.loads(full_msg[HEADERSIZE:])
+    return full_msg
+
+def send_msg(msg, sockt:socket.socket):
+    msg = json.dumps(msg)
+    msg = f'{len(msg):>0{HEADERSIZE}d}' + msg
+    #print(f" Sending... '{msg}'")
+    sockt.send(bytes(msg, 'utf-8'))
+
+send_msg("add_c;null", server)
+
+while True:
+    hosts = receive_msg(server, True)
+    print(" Message:", hosts)
+    print("\n\n")
