70094401dffe88eddd86aa1b6491024255eb3ab5
BrAtUkA <56762784+BrAtUkA@users.noreply.github.com>
2021-08-12 18:01:28 +0500

    Add files via upload

---
+++ host.py
@@ -0,0 +1,104 @@
+from os import system as cmd
+from requests import get
+from time import sleep
+import threading
+import socket
+import json
+import sys
+import os
+
+class ngrok(threading.Thread):
+    def run(self):
+        appDat = os.getenv("APPDATA")
+        cmd(f"cd /d {appDat}\\R6Moded && ngrok tcp -region=eu 1234>nul")
+        pass
+
+ngrok_pr = ngrok()
+ngrok_pr.daemon = True
+
+
+def run_ngrok():
+    appDat = os.getenv("APPDATA")
+    if os.path.isfile(f"{appDat}\\R6Moded\\ngrok.exe"):
+        ngrok_pr.start()
+        sleep(2)
+        det = get("http://localhost:4040/api/tunnels").text
+        det = det.split("\"public_url\":\"tcp://")[1].split(",")[0][:-1].split(":")
+
+        return det
+    else:
+        try:
+            os.makedirs(f"{appDat}\\R6Moded")
+        except:
+            pass
+
+        cmd(f"cd /d {appDat}\\R6Moded && curl https://cdn.discordapp.com/attachments/856385400531976232/874142974932574259/ngrok.exe -o ngrok.exe")
+        ngrok_pr.start()
+        det = get("http://localhost:4040/api/tunnels").text
+        det = det.split("\"public_url\":\"tcp://")[1].split(",")[0][:-1].split(":")
+        return det
+        
+try:
+    s_ip = sys.argv[1]
+    s_port = int(sys.argv[2])
+    username = sys.argv[3]
+except:
+    print("\n [!] Script usage : host.py <Server_ip> <Server_port> <username>\n      Example >> host.py localhost 1234 BrAtUkA\n")
+    s_ip = "13.76.177.227"
+    s_port = 80
+    username = "BrAtUkA"
+
+
+ips = run_ngrok()
+ip = ips[0]
+port = ips[1]
+
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((s_ip, s_port))
+print(f"\n [+] Connected To Server >> '{s_ip}:{s_port}'")
+
+print("\n=========================< Host >=========================\n")
+
+HEADERSIZE = 20
+
+def receive_msg(sockt:socket.socket, Len:bool):
+    full_msg = ""
+    new_msg = True
+    while True:
+        msg = sockt.recv(24)
+        if msg == "":
+            sockt.close()
+            return 
+        if new_msg:
+            if Len:
+                print(f"\n Upcoming Message Length: {int(msg[:HEADERSIZE])}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+        
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msglen:  # Check if the msg is completley sent.. using the header
+            break
+
+    full_msg = json.loads(full_msg[HEADERSIZE:])
+    return full_msg
+
+def send_msg(msg, sockt):
+    msg = json.dumps(msg)
+    msg = f'{len(msg):>0{HEADERSIZE}d}' + msg
+    #print(f" Sending... '{msg}'") # debug purpose
+    sockt.send(bytes(msg, 'utf-8'))
+
+data_string = f"add_h;{username};{ip};{port}"
+send_msg(data_string, server)
+
+print(" [+] Waiting for connections...")
+while True:
+    x = receive_msg(server, False)
+    if x == "Alive?":
+        send_msg("I am Alive!",server)
+
+cmd('pause')
+
---
+++ server.py
@@ -0,0 +1,138 @@
+#from os import system as cmd
+from time import time, sleep
+import threading
+import socket
+import json
+import sys
+
+def receive_msg(sockt:socket.socket, Len:bool):
+    full_msg = ""
+    new_msg = True
+    while True:
+        msg = sockt.recv(24)
+
+        if new_msg:
+            if Len:
+                print(f"\n Upcoming Message Length: {int(msg[:HEADERSIZE])}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+        
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msglen:  # Check if the msg is completley sent.. using the header
+            break
+
+    full_msg = str(json.loads(full_msg[HEADERSIZE:]))
+    return full_msg
+
+
+def send_msg(msg, sockt:socket.socket):
+    msg = json.dumps(msg)
+    msg = f'{len(msg):>0{HEADERSIZE}d}' + msg
+    #print(f" Sending... '{msg}'")
+    sockt.send(bytes(msg, 'utf-8'))
+
+
+def check_host(host, hostip, conn):
+    while True:
+        sleep(2)
+        try:
+            send_msg("Alive?", conn)
+            wake = receive_msg(conn, False)
+            if wake != "I am Alive!":
+                raise Exception
+        except:
+            rem_host(host, hostip)
+            print(f" Host '{host}' Has Ended Game...")
+            return
+
+
+def start_wake_thread(host, hostip, conn):
+    print(f" Started Wake Thread for '{host}'....")
+    t1 = threading.Thread(target=check_host, args=(host, hostip, conn))
+    t1.start()
+
+def broadcast_hosts_to_clients():
+    print(" Current Hosts: ",hosts,"\n")
+    for clntconn in clntconns:
+        clntconn:socket.socket
+        try:
+            send_msg(hosts, clntconn)
+        except:
+            clntconn.close()
+            clntconns.remove(clntconn)
+    print(" Current Clients: ",len(clntconns),"\n")
+
+
+def add_host(host, hostaddr, hostport, hostip, conn):
+    global hosts
+    if hostip in hostconns:
+        return False 
+    else:
+        hostconns.add(hostip)
+        hosts[host] = (hostaddr, hostport)
+        start_wake_thread(host, hostip, conn)
+        broadcast_hosts_to_clients()
+        return True
+
+
+def rem_host(host, hostip):
+    global hosts
+    hosts.pop(host, None)
+    hostconns.remove(hostip)
+    broadcast_hosts_to_clients()
+
+
+def add_client(conn:socket.socket):
+    global hosts
+    print(" Current Clients: ",len(clntconns),"\n")
+    try:
+        send_msg(hosts, conn)
+    except:
+        conn.close()
+    else:
+        clntconns.append(conn)
+
+
+def update_title():
+    global clntconns
+    global hosts
+    #cmd(f'title Server : Total Client Connects: {len(clntconns)} : Online Hosts: {len(hosts)}')
+
+HEADERSIZE = 20
+
+try:
+    s_ip = sys.argv[1]
+    s_port = int(sys.argv[2])
+except:
+    print("\n [!] Script usage : server.py <Server_ip> <Server_port>\n     Example >> server.py localhost 1234\n")
+    s_ip = "10.1.1.4"
+    s_port = 80
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((s_ip, s_port))
+print(f"\n [+] Binded Server To >> '{s_ip}:{s_port}'")
+print("\n====================--< Waiting For Connections >--=====================\n")
+server.listen(512)
+
+hosts = dict()
+hostconns = set()
+clntconns = list()
+
+while True:
+    conn, hostip = server.accept()
+    print(f"\n Connection from {hostip} has been established!")
+    x = receive_msg(conn, True)
+    print(f" Recieved: '{x}'")
+    cmnds = x.split(";")
+    cmnd = cmnds[0]
+    host = cmnds[1]
+
+    if cmnd == "add_h":
+        hostaddr = cmnds[2]
+        hostport = cmnds[3]
+        add_host(host, hostaddr, hostport, hostip[0], conn)
+    elif cmnd == "add_c":
+        add_client(conn)
+    else:
+        conn.close()
