10bfa6d7f69c122a6792988b30ced32cce970919
BrAtUkA <saadirfan856@gmail.com>
2021-08-14 14:47:51 +0500

    Prepped For Client-Host Connection

--- join.py
+++ join.py
@@ -8,6 +8,7 @@ import sys
 try:
     s_ip = sys.argv[1]
     s_port = int(sys.argv[2])
+    #hName = sys.argv[3]
 except:
     print("\n [!] Script usage : join.py <Server_ip> <Server_port>\n      Example >> join.py localhost 1234\n")
     s_ip = "13.76.177.227"
@@ -50,21 +51,32 @@ def send_msg(msg, sockt:socket.socket):
 
 send_msg("add_c;null", server)
 
-def Update_hostList():
-    global hosts
+def Recieve_hostList():
     while True:
         try:
             hosts = receive_msg(server, True)
         except:
             break
-        print(f" Hosts = {hosts}\n\n")
+        updateHostlist(hosts)
 
     print(" [!] Unexpected Error... (Server Connection Closed)")
     exit()
 
-t1 = threading.Thread(target=Update_hostList,)
+def updateHostlist(Hlis):
+    global hosts
+    hosts = Hlis
+    print(f" Hosts = {hosts}\n\n")
+
+t1 = threading.Thread(target=Recieve_hostList)
 t1.start()
 
+sleep(1)
+
+# host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# host.connect((hosts[hName][0], int(hosts[hName][1])))
+                                                             # TODO: Figure out a way to specify a host [76] [11]... ; TODO2: Add Method to connect and comunicate with specified host
 while True:
-    pass   # TODO: Figure out a way to specify a host... ; TODO2: Add Method to connect and comunicate with specified host
+    # ms = receive_msg(host, True)
+    # print(" Message = ", ms )
+    pass                                                       
 
