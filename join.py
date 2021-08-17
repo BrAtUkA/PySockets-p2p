32a1e8151c05fcf1066b63cddc6986a55532d0a1
BrAtUkA <56762784+BrAtUkA@users.noreply.github.com>
2021-08-17 10:55:25 +0500

    Added backend Embedding (Watch for main Process)

--- join.py
+++ join.py
@@ -1,9 +1,18 @@
-from os import system as cmd
+from psutil import process_iter 
+from os import getenv, _exit
 from time import sleep
 import threading
 import socket
 import json
 import sys
+
+APPDATA = getenv("APPDATA")
+Dumps = f"{APPDATA}\\\R6_Custom_Launcher1\\R6 Custom Launcher1\\1.0.0.0"
+ParentProc = "PyVBCustom.exe"
+
+if not ParentProc in (p.name() for p in process_iter()):
+    print("\n\n [!] Unexpected Startup (Main Window Not Found...)")
+    exit()
 
 try:
     s_ip = sys.argv[1]
@@ -52,31 +61,66 @@ def send_msg(msg, sockt:socket.socket):
 send_msg("add_c;null", server)
 
 def Recieve_hostList():
-    while True:
+    while ParentProc in (p.name() for p in process_iter()):
         try:
             hosts = receive_msg(server, True)
         except:
             break
         updateHostlist(hosts)
 
-    print(" [!] Unexpected Error... (Server Connection Closed)")
-    exit()
+
+def watchParentProc():
+    while True:
+        if not ParentProc in (p.name() for p in process_iter()):
+            print(" [!] Unexpected Error...")
+            server.close()
+            _exit(404)
+
 
 def updateHostlist(Hlis):
     global hosts
     hosts = Hlis
     print(f" Hosts = {hosts}\n\n")
 
-t1 = threading.Thread(target=Recieve_hostList)
+    with open(f"{APPDATA}\\\R6_Custom_Launcher1\\R6 Custom Launcher1\\1.0.0.0\\CurrentHosts.json", "w") as data:
+        data.write(json.dumps(hosts))
+
+t1 = threading.Thread(target=watchParentProc)
 t1.start()
+
+t2 = threading.Thread(target=Recieve_hostList)
+t2.start()
+
+
 
 sleep(1)
 
-# host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# host.connect((hosts[hName][0], int(hosts[hName][1])))
-                                                             # TODO: Figure out a way to specify a host [76] [11]... ; TODO2: Add Method to connect and comunicate with specified host
+
+Connected = False
+print(" [!] Waiting For Selection...")
 while True:
-    # ms = receive_msg(host, True)
-    # print(" Message = ", ms )
-    pass                                                       
+    if not(Connected):
+        try:
+            with open(f"{Dumps}\\SelectedHost.txt", "r") as hName:
+                hName = hName.read()
+
+           # print((hosts[hName][0], int(hosts[hName][1])))
+            host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            host.connect((hosts[hName][0], int(hosts[hName][1])))
+            print(f"\n >> Connected to {hName}...")
+            Connected = True
+
+        except Exception as e:
+            pass
+
+    while Connected:
+        try:
+            data = receive_msg(host, True)
+        except:
+            print(f"\n >> Disconnected From '{hName}'...")
+            print(" [!] Waiting For Selection...")
+            host.close()
+            Connected = False
+
+        print(" Message:",data)
 
